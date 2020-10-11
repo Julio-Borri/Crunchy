775a9f072803f1008e4bd740208033202f17b0a6
Julio Daniel Borri <juliodborri@gmail.com>
2020-10-11 17:20:55 -0300

    Header done (#1)

--- frontend/src/views/HomeScreen.js
+++ frontend/src/views/HomeScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Header from '../components/Header';
 
 const HomeScreen = () => {
   return (
-    <div>
-      <h1>HomeScreen</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
